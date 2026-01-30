d490b2254bc21e9b1703b927fe2a2d0356ca51ec
Darkle <120344717+Dark-IsHere@users.noreply.github.com>
2026-01-30 13:36:44 -0800

    Constants file

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,4 +4,12 @@ public class Constants {
     public static final class HardwarePorts {
         public static final int intakeID = 20;
     }
+    
+    public static final double stickDeadband = 0.075;
+    public static final double triggerDeadzone = 0.2;
+
+    public static double MaxSpeed = 6; //can be lowered during testing
+    public static double MaxAcceleration = 1.2542976; //can be lowered during testing
+    public static double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
+    public static double MaxAngularVelocity = 2 * Math.PI;
 }
