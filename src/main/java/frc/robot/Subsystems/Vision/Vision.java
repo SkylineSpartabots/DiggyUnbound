6e189b24feb366294a16917bb1ab672cd435a26d
KarthKRana <karthkrana@gmail.com>
2026-02-28 18:41:54 -0800

    Vision

---
+++ src/main/java/frc/robot/Subsystems/Vision/Vision.java
@@ -0,0 +1,25 @@
+package frc.robot.Subsystems.Vision;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Subsystems.RobotState;
+import frc.robot.Subsystems.Drivetrain.CommandSwerveDrivetrain;
+
+public class Vision extends SubsystemBase{
+    private static Vision instance;
+
+    CommandSwerveDrivetrain s_Swerve;
+    RobotState robotState;
+
+    public static Vision getInstance() {
+        if (instance == null) instance = new Vision();
+        return instance;
+    }
+
+    public Vision() {
+        s_Swerve = CommandSwerveDrivetrain.getInstance();
+        robotState = RobotState.getInstance();
+    }
+    
+
+}
